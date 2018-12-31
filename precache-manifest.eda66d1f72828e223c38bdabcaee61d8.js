571e2072076d09121b92b3aeaa79cf841c8fb6c8
Trey Hakanson <treyhakanson@gmail.com>
2018-12-30 21:37:25 -0500

    Updates

---
+++ precache-manifest.eda66d1f72828e223c38bdabcaee61d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98210810ad4725cabeb9",
+    "url": "/simple-agenda/static/css/main.77d6d5f4.chunk.css"
+  },
+  {
+    "revision": "98210810ad4725cabeb9",
+    "url": "/simple-agenda/static/js/main.98210810.chunk.js"
+  },
+  {
+    "revision": "eeddb4bb33702ce21524",
+    "url": "/simple-agenda/static/js/1.eeddb4bb.chunk.js"
+  },
+  {
+    "revision": "163cf6c843fe835406c4",
+    "url": "/simple-agenda/static/js/runtime~main.163cf6c8.js"
+  },
+  {
+    "revision": "8e26ea74477ce59d2b5b9d2ff963f13a",
+    "url": "/simple-agenda/index.html"
+  }
+];
